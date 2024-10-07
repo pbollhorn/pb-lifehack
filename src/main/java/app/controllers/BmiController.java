3db51c95bebb4d0eb1a2e2f48afcfd2a502dc833
Peter Bollhorn <pbollhorn@gmail.com>
2024-10-07 16:07:06 +0200

    bmi project is now set upped

---
+++ src/main/java/app/controllers/BmiController.java
@@ -0,0 +1,19 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class BmiController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
+    {
+        app.get("/bmi", ctx -> index(ctx, connectionPool));
+    }
+
+    private static void index(Context ctx, ConnectionPool connectionPool)
+    {
+        ctx.render("/bmi/index.html");
+    }
+
+}
